165f81086b2dffad7f03521a30028022e6f44a7f
Ayushmaan Yadav <143776285+asteriskayush007@users.noreply.github.com>
2025-01-27 23:48:52 +0530

    new features

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-const connectToDb = require('./db/db')
-const routes = require('./routes/user.routes')
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 connectToDb();
 
 app.use(cors());
-app.use(express.json);
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res)=>{ 
-    res.send("hello world")
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-app.use('/users',routes);
+app.use('/users', userRoutes);
 
 
 
--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-// Connect to MongoDB
+
 function connectToDb() {
-  mongoose
-    .connect(process.env.DB_CONNECT)
-    .then(() => console.log("Connected to DB"))
-    .catch((err) => console.log("Failed to connect to DB:", err));
+    mongoose.connect(process.env.DB_CONNECT
+    ).then(() => {
+        console.log('Connected to DB');
+    }).catch(err => console.log(err));
 }
+
 
 module.exports = connectToDb;
