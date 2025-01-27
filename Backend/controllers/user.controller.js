0da4e4b852f95b931b13f4f4e9ae08e1d9123910
Ayushmaan Yadav <143776285+asteriskayush007@users.noreply.github.com>
2025-01-28 01:03:55 +0530

    `Added cookie-parser middleware and updated routes and controllers to handle user authentication and logout.`

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blacklistedTokenModel = require('../models/blackistToken.model')
 
 module.exports.registerUser = async (req, res, next) => {
 
@@ -30,7 +31,55 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
 
     res.status(201).json({ token, user });
-    
 
 
+
+}
+
+
+module.exports.loginUser = async(req,res,next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token',token);
+
+
+    res.status(200).json({ token, user });
+}
+
+
+
+
+module.exports.getUserProfile = async (req,res,next) =>{
+    res.status(200).json(req.user);
+}
+
+
+
+
+module.exports.logoutUser = async(req,res,next)=>{
+    res.clearCookie('token');
+
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    if(token){
+        await blacklistedTokenModel.create({token});
+    }
+    res.status(200).json({message: 'Logged out'});
 }
